8f99ddb99190ff1a793d1bf5d0f50fa7ac0d953a
최성근 <ccc3132@naver.com>
2016-11-11 17:06:31 +0900

    안드로이드 추가

---
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a403.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+//add git button 추가 추가
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Hello",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
